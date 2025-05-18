3ccac2093bf7afae18bbd1e44dabdc96b725619f
plzombie <plzombie@yandex.ru>
2025-05-18 19:22:33 +0300

    Check MMX only on X86/X64

--- Sources/Engine/Engine.cpp
+++ Sources/Engine/Engine.cpp
@@ -193,7 +193,9 @@ static void DetectCPU(void)
   sys_bCPUHasCMOV = bCMOV!=0;
   sys_iCPUMHz = INDEX(_pTimer->tm_llCPUSpeedHZ/1E6);
 
+#if defined(_M_IX86) || defined(_M_X64) 
   if( !bMMX) FatalError( TRANS("MMX support required but not present!"));
+#endif
 }
 
 static void DetectCPUWrapper(void)
